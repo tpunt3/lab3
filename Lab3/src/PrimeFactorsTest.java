f83a76e3e23f8608a5ad619a01ad99f7a884b322
Trent Punt <tpunt3@gmail.com>
2015-03-24 18:42:08 -0400

    Test 325115
    added test 325115

--- Lab3/src/PrimeFactorsTest.java
+++ Lab3/src/PrimeFactorsTest.java
@@ -5,46 +5,49 @@ import java.util.List;
 
 import org.junit.Test;
 
-
 public class PrimeFactorsTest {
 
-	private List<Integer> list(Integer...integers) {
+	private List<Integer> list(Integer... integers) {
 		return Arrays.asList(integers);
 	}
-	
+
 	@Test
 	public void testOne() {
 		assertEquals(list(), PrimeFactors.generate(1));
 	}
-	
+
 	@Test
 	public void testTwo() throws Exception {
-		assertEquals(list(2),PrimeFactors.generate(2));
+		assertEquals(list(2), PrimeFactors.generate(2));
 	}
-	
+
 	@Test
 	public void testThree() throws Exception {
-		assertEquals(list(3),PrimeFactors.generate(3));
+		assertEquals(list(3), PrimeFactors.generate(3));
 	}
-	
+
 	@Test
 	public void testFour() throws Exception {
-		assertEquals(list(2,2),PrimeFactors.generate(4));
+		assertEquals(list(2, 2), PrimeFactors.generate(4));
 	}
-	
+
 	@Test
 	public void testSix() throws Exception {
-		assertEquals(list(2,3),PrimeFactors.generate(6));
+		assertEquals(list(2, 3), PrimeFactors.generate(6));
 	}
-	
+
 	@Test
 	public void testEight() throws Exception {
-		assertEquals(list(2,2,2),PrimeFactors.generate(8));
+		assertEquals(list(2, 2, 2), PrimeFactors.generate(8));
 	}
-	
+
 	@Test
 	public void testNine() throws Exception {
-		assertEquals(list(3,3),PrimeFactors.generate(9));
+		assertEquals(list(3, 3), PrimeFactors.generate(9));
 	}
-	
+
+	@Test
+	public void test325115() throws Exception {
+		assertEquals(list(5, 7, 7, 1327), PrimeFactors.generate(325115));
+	}
 }
