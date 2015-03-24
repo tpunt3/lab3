d980e75b279747227a79a8ef3f004e73dbdcb8a9
Trent Punt <tpunt3@gmail.com>
2015-03-24 18:27:20 -0400

    testFour
    Created test four and then created the code to make it work

--- Lab3/src/PrimeFactors.java
+++ Lab3/src/PrimeFactors.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class PrimeFactors {
 
 	public static List<Integer> generate(int i) {
-		// TODO Auto-generated method stub
 		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if(i % 2==0){
+			primes.add(2);
+			i= i/2;
+		}
 		if(i > 1){
 			primes.add(i);
 		}
