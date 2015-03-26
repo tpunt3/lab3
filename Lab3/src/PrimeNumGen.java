867cb9a91f4e6de9aaed225b229e8631fdf25234
Trent Punt <tpunt3@gmail.com>
2015-03-26 16:15:12 -0400

    Fixed test two
    wrote code to fix test two

--- Lab3/src/PrimeNumGen.java
+++ Lab3/src/PrimeNumGen.java
@@ -4,6 +4,9 @@ public class PrimeNumGen {
 
 	public ArrayList generate(Integer input) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if(input > 2){
+			primes.add(2);
+		}
 		return primes;
 	}
 
