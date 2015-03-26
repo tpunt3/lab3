e1b66ffbabeff65282c0e722c1c399dbb6d012e1
Trent Punt <tpunt3@gmail.com>
2015-03-26 16:20:43 -0400

    Fixed test three
    wrote code to fix test three

--- Lab3/src/PrimeNumGen.java
+++ Lab3/src/PrimeNumGen.java
@@ -4,8 +4,13 @@ public class PrimeNumGen {
 
 	public ArrayList generate(Integer input) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if(input > 2){
+		if(input <= 2){
+			return primes;
+		}else{
 			primes.add(2);
+			if (input >3){
+				primes.add(3);
+			}
 		}
 		return primes;
 	}
